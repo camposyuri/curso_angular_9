99323b5af387bb6cbf52a5f685a9b73fd6a4d2e6
camposyuri <yurinapoleao@gmail.com>
2020-05-25 15:29:45 -0300

    [ADD] - Navegando entre Componentes

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatListModule } from "@angular/material/list";
 import { HomeComponent } from "./views/home/home.component";
 
 import { MatCardModule } from "@angular/material/card";
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { MatCardModule } from "@angular/material/card";
     FooterComponent,
     NavComponent,
     HomeComponent,
+    ProductCrudComponent,
   ],
   imports: [
     BrowserModule,
