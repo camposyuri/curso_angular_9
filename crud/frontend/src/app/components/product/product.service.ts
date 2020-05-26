170bf15d2ce448b863955fe719da703e223606e5
camposyuri <yurinapoleao@gmail.com>
2020-05-26 14:36:35 -0300

    [ADD] - Excluir produto

--- crud/frontend/src/app/components/product/product.service.ts
+++ crud/frontend/src/app/components/product/product.service.ts
@@ -3,7 +3,6 @@ import { MatSnackBar } from "@angular/material/snack-bar";
 import { HttpClient } from "@angular/common/http";
 import { Product } from "./product.model";
 import { Observable } from "rxjs";
-import { throwToolbarMixedModesError } from "@angular/material/toolbar";
 
 @Injectable({
   providedIn: "root",
@@ -37,5 +36,10 @@ export class ProductService {
   update(product: Product): Observable<Product> {
     const url = `${this.baseUrl}/${product.id}`;
     return this.http.put<Product>(url, product);
+  }
+
+  delete(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Product>(url);
   }
 }
